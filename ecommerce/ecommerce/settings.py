00b7560d221a17afc2f63807b5f895c7711d089f
ngecu <devngecu@gmail.com>
2020-05-30 21:52:42 +0300

    data structuring all good

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -118,8 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,'static')
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
+from .models import *
 
 def store(request):
-     context = {}
+     products = Product.objects.all()
+     context = {"products":products}
      return render(request, 'store/Store.html', context)
 
 def cart(request):
-     context = {}
+     #check if authenticated
+     if request.user.is_authenticated:
+          customer = request.user.customer
+          order,created = Order.objects.get_or_create(customer=customer,complete=False)
+          items = order.orderitem_set.all() 
+     else:
+          items = []
+          order ={'get_cart_total':0,'get_cart_items':0}
+     
+     context = {"items":items,"order":order}
      return render(request, 'store/Cart.html', context)
 
 def checkout(request):
-      context = {}
-      return render(request, 'store/Checkout.html', context)
+
+     if request.user.is_authenticated:
+          customer = request.user.customer
+          order,created = Order.objects.get_or_create(customer=customer,complete=False)
+          items = order.orderitem_set.all() 
+     else:
+          items = []
+          order ={'get_cart_total':0,'get_cart_items':0}
+
+     
+     context = {"items":items,"order":order}
+     return render(request, 'store/Checkout.html', context)
