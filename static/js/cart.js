c8393c7abbea2ad61265a6e7fa5d89f73946f362
ngecu <devngecu@gmail.com>
2020-11-10 18:54:47 +0300

    cart quantity successful

--- static/js/cart.js
+++ static/js/cart.js
@@ -34,5 +34,6 @@ function updateUserOrder(productId,action) {
     })
     .then((data) => {
          console.log("data:",data)
+         location.reload()
     })
 }
--- store/views.py
+++ store/views.py
@@ -8,20 +8,35 @@ from .models import *
 # Create your views here.
 
 def store(request):
-     products = Product.objects.all()
-     context = {'products':products}
-     return render(request, 'store/store.html', context)
 
-def cart(request):
      if request.user.is_authenticated:
         customer = request.user.customer
         order,created = Order.objects.get_or_create(customer=customer,complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
      else:
           items = []
           order = {'get_cart_total':0,'get_cart_items':0}
+          cartItems = order['get_cart_items']
 
-     context = {'items':items,'order':order}
+     products = Product.objects.all()
+     context = {'products':products,"cartItems":cartItems}
+     return render(request, 'store/store.html', context)
+
+def cart(request):
+
+     if request.user.is_authenticated:
+        customer = request.user.customer
+        order,created = Order.objects.get_or_create(customer=customer,complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+
+     else:
+          items = []
+          order = {'get_cart_total':0,'get_cart_items':0}
+          cartItems = order['get_cart_items']
+
+     context = {'items':items,'order':order,"cartItems":cartItems}
      return render(request, 'store/cart.html', context)
 
 def checkout(request):
@@ -29,11 +44,14 @@ def checkout(request):
         customer = request.user.customer
         order,created = Order.objects.get_or_create(customer=customer,complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+
      else:
           items = []
           order = {'get_cart_total':0,'get_cart_items':0}
+          cartItems = order['get_cart_items']
           
-     context = {'items':items,'order':order}
+     context = {'items':items,'order':order,"cartItems":cartItems}
      return render(request, 'store/checkout.html', context)
 
 
