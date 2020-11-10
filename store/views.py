e2426f302285d54c523d736db7f5d8475aba170a
ngecu <devngecu@gmail.com>
2020-11-10 18:44:56 +0300

    adding to cart successfully

--- store/views.py
+++ store/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+
+from django.http import JsonResponse
+
+import json
+
 from .models import *
 # Create your views here.
 
@@ -30,3 +35,30 @@ def checkout(request):
           
      context = {'items':items,'order':order}
      return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+     data = json.loads(request.body)
+     productId = data['productId']
+     action = data['action']
+
+     print("productId:",productId)
+     print("Action:",action)
+
+     customer = request.user.customer
+     product = Product.objects.get(id=productId)
+     order,created = Order.objects.get_or_create(customer=customer,complete=False)
+
+     orderItem,created = OrderItem.objects.get_or_create(order=order,product=product)
+
+     if action == "add":
+          orderItem.quantity = (orderItem.quantity + 1)
+     elif action == 'remove':
+          orderItem.quantity = (orderItem.quantity - 1)
+
+     orderItem.save()
+
+     if orderItem.quantity <=0:
+          orderItem.delete()
+
+     return JsonResponse('Item was added',safe=False)
