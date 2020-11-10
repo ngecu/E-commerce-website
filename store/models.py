44f8465c3b8092fe0d7398e5418b9e5342c13168
ngecu <devngecu@gmail.com>
2020-11-10 04:37:56 +0300

    completed data structure module

--- store/models.py
+++ store/models.py
@@ -24,7 +24,7 @@ class Product(models.Model):
             url = self.image._unregister_lookup
         except:
             url = ''
-            
+
         return url
 
 class Order(models.Model):
@@ -36,6 +36,17 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product,on_delete=models.SET_NULL,null=True)
@@ -43,6 +54,10 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0,null=True,blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
